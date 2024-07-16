7696a16f953ef02c3f0886e5554cdb2929ef2aa2
avilasebastianl <avilasebastianl@hotmail.com>
2024-07-16 10:18:36 -0500

    Archivo paraguardas las rutas relativas de carpetas dentro del proyecto

---
+++ src/paths.py
@@ -0,0 +1,8 @@
+import os
+script_directory  = os.path.dirname(os.path.abspath(__file__))
+project_directory = os.path.dirname(script_directory)
+path_to_config    = os.path.join(project_directory,"config")
+path_to_logs      = os.path.join(project_directory,"log")
+path_to_data      = os.path.join(project_directory,"data")
+path_to_docs      = os.path.join(project_directory,"docs")
+path_to_sql       = os.path.join(project_directory,"sql")
