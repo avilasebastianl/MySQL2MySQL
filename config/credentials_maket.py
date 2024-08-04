0c8b60624bd356b7a7495e632f94186636d6d9a0
avilasebastianl <avilasebastianl@hotmail.com>
2024-08-04 02:13:38 -0500

    Dinamismo de insercion al diccionario matriz de usuario y contraseña en lo servidores que administra el desarrollador

--- config/credentials_maket.py
+++ config/credentials_maket.py
@@ -1,3 +1,5 @@
+big_data_servers: list[str] = []
+
 dict_user: dict[str:str] = {
     'servidor1'   : 'usuario1',
     'servidor2'   : 'usuario2'
@@ -7,4 +9,11 @@ dict_pwd: dict[str:str] = {
     'servidor2'   : 'contraseña2'
 }
 
-__BIGDATA_SERVERS__: list[str]= []
+admin_user: str = ''
+admin_pswd: str = ''
+admin_bbdd: str = ''
+admin_port: int = ''
+
+for server in big_data_servers:
+    dict_user[server] = admin_user
+    dict_pwd[server]  = admin_pswd
