8046efd6d5a039a9291e8d28d2f83a932ac3cadb
Sebastian Avila <89407007+avilasebastianl@users.noreply.github.com>
2024-10-20 18:53:58 -0500

    Update: Mejora de estructura de codigo basado en pep8 y todo retorna tipo path

--- src/paths.py
+++ src/paths.py
@@ -1,8 +1,31 @@
+"""
+Modulo de rutas constantes de cada carpeta del proyectos
+"""
 import os
-script_directory  = os.path.dirname(os.path.abspath(__file__))
-project_directory = os.path.dirname(script_directory)
-path_to_config    = os.path.join(project_directory,"config")
-path_to_logs      = os.path.join(project_directory,"log")
-path_to_data      = os.path.join(project_directory,"data")
-path_to_docs      = os.path.join(project_directory,"docs")
-path_to_sql       = os.path.join(project_directory,"sql")
+from pathlib import Path
+
+script_directory:Path = Path(__file__).parent.absolute()
+project_directory:Path = script_directory.parent
+path_to_config:Path = project_directory / "config"
+path_to_logs:Path = project_directory / "log"
+path_to_data:Path = project_directory / "data"
+path_to_docs:Path = project_directory / "docs"
+path_to_bin:Path = project_directory / "bin"
+path_to_sql:Path = project_directory / "sql"
+config_path:Path = project_directory / ".conf"
+path_to_share:Path = path_to_sql / "share"
+
+def join_func(prefix:Path,sufix:str) -> Path:
+    """Funcion para unir strings y retornar una ruta en especifico
+
+    Args:
+        prefix (Path): Ruta definida a una carpeta.
+        sufix (str): Nombre del archivo a unir.
+
+    Returns:
+        Path: Ruta final del archivo
+    """
+    return Path(os.path.join(prefix,sufix))
+
+if __name__ == '__main__':
+    pass # ! No borre esta linea de codigo, realice pruebas abajo
