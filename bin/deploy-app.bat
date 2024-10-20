7eaa00213cbc8847c0ed62fe3f094d3130e927d3
Sebastian Avila <89407007+avilasebastianl@users.noreply.github.com>
2024-10-20 18:50:00 -0500

    Add: Archivo de ejecucion para despliegue de aplicacion (Linux y Windows)

---
+++ bin/deploy-app.bat
@@ -0,0 +1,47 @@
+@echo off
+echo Deploying ETL MySQL2MySQL
+
+set current_path=%cd%
+cd %current_path%
+cd ..
+
+type docs\devs.txt
+mkdir config
+echo config folder created
+mkdir data
+echo data folder created
+mkdir logs
+echo logs folder created
+
+where py >nul 2>&1
+if %errorlevel%==0 (
+    set PYTHON_COMMAND=py
+) else (
+    where python >nul 2>&1
+    if %errorlevel%==0 (
+        set PYTHON_COMMAND=python
+    ) else (
+        echo No Python interpreter found.
+        exit /b 1
+    )
+)
+echo Python command found: "%PYTHON_COMMAND%"
+
+echo Creating virtual enviroment
+%PYTHON_COMMAND% -m venv venv --upgrade-deps
+call venv\Scripts\activate
+echo Installing libraries
+pip install -r requirements.txt
+echo Libraries:
+pip list
+%PYTHON_COMMAND% src\deploy.py
+
+choice /M "Do you want to exit..."
+if %errorlevel%==1 (
+    echo Bye...
+    timeout /t 2 /nobreak 
+    exit /b 0
+) else (
+    echo Check results...
+    pause
+)
---
+++ bin/deploy-app.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "Deploying ETL MySQL2MySQL"
+
+current_path=$(pwd)
+cd "$current_path"
+cd ..
+
+cat docs/devs.txt
+mkdir config
+echo config folder created
+mkdir data
+echo data folder created
+mkdir logs
+echo logs folder created
+
+if command -v py >/dev/null 2>&1; then
+    PYTHON_COMMAND=py
+elif command -v python >/dev/null 2>&1; then
+    PYTHON_COMMAND=python
+else
+    echo "No Python interpreter found."
+    exit 1
+fi
+
+echo "Python command found: $PYTHON_COMMAND"
+
+echo "Creating virtual environment"
+$PYTHON_COMMAND -m venv venv --upgrade-deps
+
+source venv/bin/activate
+
+echo "Installing libraries"
+pip install -r requirements.txt
+
+echo "Libraries:"
+pip list
+
+$PYTHON_COMMAND src/deploy.py
+
+read -p "Do you want to exit? (y/n): " choice
+if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+    echo "Bye..."
+    sleep 2
+    exit 0
+else
+    echo "Check results..."
+    read -p "Press Enter to continue..."
+fi
