3835a23931d71332f55b82c8b6b6f297335938ad
Sebastian Avila <89407007+avilasebastianl@users.noreply.github.com>
2024-10-20 20:32:43 -0500

    Fix: comando de python arreglado

--- bin/deploy-app.sh
+++ bin/deploy-app.sh
@@ -13,19 +13,8 @@ echo data folder created
 mkdir log
 echo logs folder created
 
-if command -v py >/dev/null 2>&1; then
-    PYTHON_COMMAND=py
-elif command -v python >/dev/null 2>&1; then
-    PYTHON_COMMAND=python
-else
-    echo "No Python interpreter found."
-    exit 1
-fi
-
-echo "Python command found: $PYTHON_COMMAND"
-
 echo "Creating virtual environment"
-$PYTHON_COMMAND -m venv venv --upgrade-deps
+python3 -m venv venv
 
 source venv/bin/activate
 
@@ -35,8 +24,8 @@ pip install -r requirements.txt
 echo "Libraries:"
 pip list
 
-$PYTHON_COMMAND src/deploy.py
-$PYTHON_COMMAND Main.pyw -h
+python3 src/deploy.py
+python3 Main.pyw -h
 
 read -p "Do you want to exit? (y/n): " choice
 if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
